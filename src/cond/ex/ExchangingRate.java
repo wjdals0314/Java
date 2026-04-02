0b76901ecd991e1582d758ab2d283d5a4c04db63
홍정민 <s01046927968@gmail.com>
2026-04-02 16:11:42 +0900

    java 숙제

---
+++ src/cond/ex/ExchangingRate.java
@@ -0,0 +1,17 @@
+package cond.ex;
+
+public class ExchangingRate {
+    static void main() {
+        int dollar=100, won;
+        if (dollar<0){
+            System.out.println("잘못된 금액입니다.");
+        }
+        else if (dollar==0){
+            System.out.println("환전할 금액이 없습니다.");
+        }
+        else{
+            won=dollar*1300;
+            System.out.println("화전 금액은 "+won+"원 입니다.");
+        }
+    }
+}
