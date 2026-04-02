5d1f9efeea97fdf3f07dd1e85e7e47298ba3ac49
홍정민 <s01046927968@gmail.com>
2026-04-02 20:20:47 +0900

    java 숙제

---
+++ src/loop/While1_2.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class While1_2 {
+    static void main() {
+         int count=0;
+
+         while (count<3){
+             count+=1;
+             System.out.println("현재 숫자는 "+count+"입니다.");
+         }
+    }
+}
