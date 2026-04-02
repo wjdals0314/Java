2ff4ef4cbdcde39848e0967e05654157e61f8160
홍정민 <s01046927968@gmail.com>
2026-04-02 16:17:40 +0900

    java 숙제

---
+++ src/cond/ex/MoveRateEx.java
@@ -0,0 +1,17 @@
+package cond.ex;
+
+public class MoveRateEx {
+    static void main() {
+        double rating=7;
+
+        if (rating<=9){
+            System.out.println("'어바웃타임'을 추천합니다.");
+        }
+        if (rating<=8) {
+            System.out.println("'토이스토리'를 추천합니다.");
+        }
+        if (rating<=7){
+            System.out.println("'고질라'를 추천합니다.");
+        }
+    }
+}
