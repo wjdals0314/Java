e086dfbf62cc01dde6efc390a30f75c6e3ba274c
홍정민 <s01046927968@gmail.com>
2026-04-06 17:43:16 +0900

    java 숙제

--- src/scanner/ScannerWhile1.java
+++ src/scanner/ScannerWhile1.java
@@ -1,4 +1,19 @@
 package scanner;
 
+import java.util.Scanner;
+
 public class ScannerWhile1 {
+    static void main() {
+        Scanner scanner=new Scanner(System.in);
+
+        while (true) {
+            System.out.print("문자열을 입력하세요(exit: 종료):");
+            String str=scanner.nextLine();
+            if (str.equals( "exit")) {
+                System.out.println("입력을 종료합니다.");
+                break;
+            }
+            System.out.println("입력한 문자열: "+str);
+        }
+    }
 }
